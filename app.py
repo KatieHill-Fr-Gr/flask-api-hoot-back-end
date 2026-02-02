11acd134773907a968b73c4f457eb156dcdda8cf
Katie Hill <khill.fr.gr@gmail.com>
2026-02-02 15:37:41 +0000

    Post comment route added

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from dotenv import load_dotenv
 from auth_blueprint import authentication_blueprint
 from hoots_blueprint import hoots_blueprint
+from comments_blueprint import comments_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(authentication_blueprint)
 app.register_blueprint(hoots_blueprint)
+app.register_blueprint(comments_blueprint)
 
 # Run our application, by default on port 5000
 app.run()
