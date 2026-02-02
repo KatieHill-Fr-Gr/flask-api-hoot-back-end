78504d33b6c8913e3a6c40d20bb463f63eeb7dc0
Katie Hill <khill.fr.gr@gmail.com>
2026-02-02 14:32:31 +0000

    comments blueprint added

---
+++ comments_blueprint.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, jsonify, request, g
+from db_helpers import get_db_connection
+import pscycopg2.extras
+from auth_middleware import token_required
+
+comments_blueprint = Blueprint('comments_blueprint', __name__)
